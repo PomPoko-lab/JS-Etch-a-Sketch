654c9b62f0a556e0261380eff509ee4ac58aeec6
ckennx <ckennx@gmail.com>
2022-01-11 13:18:02 -0600

    Finished scripts for grid

--- game.js
+++ game.js
@@ -1,13 +1,38 @@
 const sketchGrid = document.querySelector('.sketch-box');
 const sketchSquare = document.createElement('div');
+const button = document.querySelector('button');
 
+const createSquare = parent => {
+    const square = document.createElement('div');
+    square.classList.add('square');
+    parent.append(square);
+}
 
-const size = 2; // Remove! For testing gridSize.
-let gridSize;
+const resetSquare = parent => {
+    sketchGrid.innerHTML = '';
+}
 
-// for (let i = 0; i < size; i++;) {
-//     sketchSquare;
-// }
+const addGridSquares = num => {
+    for (let i = 0; i < (num * num); i++) {
+        createSquare(sketchGrid);
+    }
+}
 
-sketchGrid.appendChild(sketchSquare);
-sketchGrid.appendChild(sketchSquare);
+const editGridSize = num => {
+    sketchGrid.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
+    sketchGrid.style.gridTemplateRows = `repeat(${num}, 1fr)`;
+};
+
+const increaseSize = (oldSize, newSize) => {
+    addGridSquares(oldSize, newSize);
+    editGridSize(newSize);
+}
+
+button.addEventListener('click', () => {
+    size = prompt('What would you like the size to be? ');
+    resetSquare(sketchGrid);
+    addGridSquares(size);
+    editGridSize(size);
+})
+
+let size;
