c5edd88510f5435c287cf77edee508c087751725
ckennx <ckennx@gmail.com>
2022-01-11 11:45:25 -0600

    Grid setup

---
+++ game.js
@@ -0,0 +1,13 @@
+const sketchGrid = document.querySelector('.sketch-box');
+const sketchSquare = document.createElement('div');
+
+
+const size = 2; // Remove! For testing gridSize.
+let gridSize;
+
+// for (let i = 0; i < size; i++;) {
+//     sketchSquare;
+// }
+
+sketchGrid.appendChild(sketchSquare);
+sketchGrid.appendChild(sketchSquare);
